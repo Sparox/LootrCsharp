9b44bf5e8656c28a5b25da00769fc2bc1fcdab5b
Laurent Palaro <laurent.palaro@outlook.com>
2014-07-31 14:09:39 +0200

    Added getAllItems method

--- Lootr.cs
+++ Lootr.cs
@@ -1,89 +1,104 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Linq;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-public class Lootr
+namespace LootrConsole
 {
+    public class Lootr
+    {
 
-	private string name;
-	private List<Object> items = new List<Object>();
-	private List<Lootr> branchs = new List<Lootr>();
-	private List<Object> nameModifiers = new List<Object>();
+        private string name;
+        private List<Object> items = new List<Object>();
+        private List<Lootr> branchs = new List<Lootr>();
+        private List<Object> nameModifiers = new List<Object>();
 
-	public Lootr(string name)
-	{
-		var branchName = name;
+        public Lootr(string name)
+        {
+            var branchName = name;
 
-		branchName = this.clean(branchName);
-		if (name.IndexOf('/') > -1)
-		{
-			throw new Exception("Specified name should not contain a / separator");
-		}
-		this.name = branchName;
+            branchName = this.clean(branchName);
+            if (name.IndexOf('/') > -1)
+            {
+                throw new Exception("Specified name should not contain a / separator");
+            }
+            this.name = branchName;
 
-	}
+        }
 
-	private string clean(string name)
-	{
-		return name.Trim();
-	}
+        private string clean(string name)
+        {
+            return name.Trim();
+        }
 
-	public Lootr add(Object item, string path)
-	{
-		if (path.Count() == 0)
-		{
-			this.items.Add(item);
-		}
-		else
-		{
-			var branch = this.branch(path);
-			branch.items.add(item);
-		}
+        public Lootr add(Object item, string path)
+        {
+            if (path.Count() == 0)
+            {
+                this.items.Add(item);
+            }
+            else
+            {
+                var branch = this.branch(path);
+                branch.items.Add(item);
+            }
 
-		return this;
-	}
+            return this;
+        }
 
-	public Lootr branch(string name)
-	{
-		return this.getBranch(name, true);
-	}
+        public Lootr branch(string name)
+        {
+            return this.getBranch(name, true);
+        }
 
-	public Lootr getBranch(string name, bool create)
-	{
-		var path = this.clean(name).Split('/');
+        public Lootr getBranch(string name, bool create)
+        {
+            var path = this.clean(name).Split('/');
 
-		if (!this.branchs.Any(b => b.name == path[0])
-		   && path[0] != this.name
-		   && create)
-		{
-			this.branchs.Add(new Lootr(path[0]));
-		}
+            if (!this.branchs.Any(b => b.name == path[0])
+               && path[0] != this.name
+               && create)
+            {
+                this.branchs.Add(new Lootr(path[0]));
+            }
 
-		if (path.Length == 1)
-		{
-			return path[0] == this.name ? this : this.branchs.FirstOrDefault(b => b.name == path[0]);
-		}
-		else if (path.Length > 1)
-		{
-			var head = path.Take(1).ToArray()[0];
-			path = path.Skip(1).ToArray();
-			var newPath = string.Join("/", path);
+            if (path.Length == 1)
+            {
+                return path[0] == this.name ? this : this.branchs.FirstOrDefault(b => b.name == path[0]);
+            }
+            else if (path.Length > 1)
+            {
+                var head = path.Take(1).ToArray()[0];
+                path = path.Skip(1).ToArray();
+                var newPath = string.Join("/", path);
 
-			if (this.branchs.Any(b => b.name == head))
-			{
-				return this.branchs.First(b => b.name == head).getBranch(newPath, create);
-			}
+                if (this.branchs.Any(b => b.name == head))
+                {
+                    return this.branchs.First(b => b.name == head).getBranch(newPath, create);
+                }
 
-			if (create) 
-			{
-				this.branchs.Add(new Lootr(head));
-				return this.branchs.First(b => b.name == head).getBranch(newPath, create);
-			}
-		}
+                if (create)
+                {
+                    this.branchs.Add(new Lootr(head));
+                    return this.branchs.First(b => b.name == head).getBranch(newPath, create);
+                }
+            }
 
-		return null;
-	}
+            return null;
+        }
 
+        public List<Object> getAllItems()
+        {
+            var allItems = new List<Object>(this.items);
+
+            foreach (var branch in this.branchs)
+            {
+                allItems.AddRange(branch.getAllItems());
+            }
+
+            return allItems;
+        }
+
+    }
 }
